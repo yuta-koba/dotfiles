3af400c5911877a8627bc5e61cc99709278b9778
lunarxlark <lunarxlark@gmail.com>
2018-12-02 23:29:23 +0900

    mod

--- .profile.d/PS1.sh
+++ .profile.d/PS1.sh
@@ -1,16 +1,27 @@
-PS1="\[\e[1;36m\]\u\[\e[0m\]"
-PS1+="\[\e[1;31m\]@\[\e[0m\]"
-PS1+="\[\e[1;33m\]\h\[\e[0m\] "
+# Normal
+Yellow='\[\e[33m\]'
+Sky='\[\e[36m\]'
+
+# Bold
+BRed='\[\e[1;31m\]'
+BYellow='\[\e[1;33m\]'
+BSky='\[\e[1;36m\]'
+Reset='\[\e[0m\]'
+
+
+PS1="${BSky}\u${Reset}"
+PS1+="${BRed}@${Reset}"
+PS1+="${BYellow}\h${Reset} "
 PS1+="\W"
-PS1+="\[\e[33m\]\$(__git_ps1)\[\e[0m\] "
-PS1+="\[\e[36m\][${AWS_PROFILE:-none}]\[\e[0m\] "
-PS1+="\[\e[1;31m\][\[\e[0m\]"
+PS1+="${BYellow}\$(__git_ps1)${Reset} "
+PS1+="${Sky}[${AWS_PROFILE:-none}]${Reset} "
+PS1+="${BRed}[${Reset}"
 PS1+="\d"
-PS1+="\[\e[1;31m\]|\[\e[0m\]"
+PS1+="${BRed}|${Reset}"
 PS1+="\t"
-PS1+="\[\e[1;31m\]]\[\e[0m\]"
+PS1+="${BRed}]${Reset}"
 PS1+="\n"
-PS1+="\[\e[1;36m\](*'-')\[\e[0m\] "
-PS1+="\[\e[1;31m\]<\[\e[0m\]"
-PS1+="\[\e[1;33m\]<\[\e[0m\]"
-PS1+="\[\e[1;36m\]<\[\e[0m\] "
+PS1+="${BSky}(*'-')${Reset} "
+PS1+="${BRed}<${Reset}"
+PS1+="${BYellow}<${Reset}"
+PS1+="${BSky}<${Reset} "
