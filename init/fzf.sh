75296302215ad9b52b1ea3df96bcc5406770ca59
lunarxlark <lunarxlark@lunarxlarks-MacBook-Pro.local>
2018-12-06 00:38:29 +0900

    fix

--- init/fzf.sh
+++ init/fzf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/junegunn/fzf.git ~/src/github.com/junegunn/fzf
-cd ~/src/github.com/junegunn/fzf
+git clone https://github.com/junegunn/fzf.git $GHQ_ROOT/github.com/junegunn/fzf
+cd $GHQ_ROOT/github.com/junegunn/fzf
 bash install --xdg --key-bindings --completion --update-rc
 rm ~/.zshrc
