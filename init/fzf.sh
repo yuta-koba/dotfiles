23071017eaa33ad42662fe55ceb053507707471a
lunarxlark <lunarxlark@gmail.com>
2019-03-26 01:18:25 +0900

    mod init/

--- init/fzf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/junegunn/fzf.git "${GHQ_ROOT}"/github.com/junegunn/fzf
-cd "${GHQ_ROOT}"/github.com/junegunn/fzf || return
-bash install --xdg --key-bindings --completion --update-rc
-rm ~/.zshrc
